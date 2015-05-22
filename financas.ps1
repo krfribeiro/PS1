ba685ca06f7abef40a7ad3dec24531994b49e8f7
Kik0 <ricardoftribeiro@gmail.com>
2015-05-22 16:36:17 +0100

    $Estado enviado via Popup
    Adicionada a funcionalidade de criação de popup com o resultado do espaço
    # To do -> Minimizar o $IE :)

--- financas.ps1
+++ financas.ps1
@@ -1,31 +1,45 @@
-# PARA QUEM ESTÁ FARTO DE FAZER LOGIN NO SITE DAS FINANÇAS PARA SABER SE JA RECEBEU OU NAO! #
-# FAZ-SE UM SCRIPT PARA LA IR VER #
-# UM POBRE A AJUDAR POBRES :P
+# by KikO!
+#
+# Script para ver se já recebemos ou não do IRS!
+#
+# Porque o pobre não precisa mais de fazer F5 de 10 em 10 minutos no site das finanças!
+#
+# É necessário mais abaixo editar o NIF e Password conforme :)
+#
 
 $URI = "https://www.acesso.gov.pt/jsp/loginRedirectForm.jsp?path=consultarDeclaracoesIRS.action&partID=M3SV"
 
 $ie = New-Object -com InternetExplorer.Application
-# Se meter $False estoira por nao carregar como deve ser as livrarias de Javascript! Whoops!
-# Se conseguirem resolver a cena! Be my guest :)
 $ie.visible=$true
 $ie.navigate("$URI")
-while($ie.ReadyState -ne 4) {start-sleep -m 100}
 
-# ALTERAR AQUI OS DADOS PARA LOGIN #
+while($ie.ReadyState -ne 4) { start-sleep -m 1000 }
+
+# Edit here only :)
 
 $ie.document.getElementById('username').value = "NIF"
-$ie.document.getElementById('password').value = "Password"
-
-# NAO ALTERAR NADA A PARTIR DAQUI :D #
+$ie.document.getElementById('password').value = "PASSWORD"
 
 $ie.document.getElementById('sbmtLogin').Click()
-start-sleep -m 5000
+start-sleep -m 6000
+
 $ie.document.getElementById('pesquisar').Click()
+
 start-sleep -m 1000
+
 ($ie.Document.links | ? { $_.href -like '*detalheDeclaracaoIRS*' }).Click()
+
 start-sleep -m 1000
+
 $estado = $ie.Document.getElementById('dataRececao').IHTMLInputElement_value
+
+$wshell = New-Object -ComObject Wscript.Shell
+$ie.Quit()
+
 if ($estado -notlike 'REEMBOLSO EMITIDO') {
-    echo "You're still poor! - $ESTADO " }
-Else { echo "You're probably rich! - $ESTADO" }
-#$ie.Quit() 
+    
+    (new-object -ComObject wscript.shell).Popup("You're still poor! - $ESTADO ",0,"Done")
+}
+Else { (new-object -ComObject wscript.shell).Popup("You're probably rich! - $ESTADO",0,"Done") }
+
+exit
