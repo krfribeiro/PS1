a1700e80e51609f64cf4840337fa02918e5f9df4
Kik0 <ricardoftribeiro@gmail.com>
2016-04-18 12:35:19 +0100

    Update financas.ps1
    Edited for Win10 support :)

--- financas.ps1
+++ financas.ps1
@@ -6,7 +6,6 @@
 #
 
 $URI = "https://www.acesso.gov.pt/jsp/loginRedirectForm.jsp?path=consultarDeclaracoesIRS.action&partID=M3SV"
-
 $ie = New-Object -com InternetExplorer.Application
 $ie.visible=$true
 $ie.navigate("$URI")
@@ -14,28 +13,20 @@ $ie.navigate("$URI")
 while($ie.ReadyState -ne 4) { start-sleep -m 1000 }
 
 $ie.document.getElementById('username').value = "NIF"
-$ie.document.getElementById('password').value = "PASSWORD"
-
+$ie.document.getElementById('password').value = "Password"
 $ie.document.getElementById('sbmtLogin').Click()
-start-sleep -m 6000
-
+start-sleep -m 3000
 $ie.document.getElementById('pesquisar').Click()
-
 start-sleep -m 1000
-
 ($ie.Document.links | ? { $_.href -like '*detalheDeclaracaoIRS*' }).Click()
-
 start-sleep -m 1000
-
-$estado = $ie.Document.getElementById('dataRececao').IHTMLInputElement_value
-
+# Win10
+$estado = $ie.Document.IHTMLDocument3_getElementById('dataRececao').defaultValue
+# Win7+
+# $estado = $ie.Document.getElementById('dataRececao').IHTMLInputElement_value
 $wshell = New-Object -ComObject Wscript.Shell
 $ie.Quit()
-
 if ($estado -like '*REEMBOLSO EMITIDO*') {
-    
     (new-object -ComObject wscript.shell).Popup("You're probably rich! - $ESTADO",0,"IRSState | K")
 }
 Else { (new-object -ComObject wscript.shell).Popup("You're still poor! - $ESTADO ",0,"IRSState | K") }
-
-# exit
